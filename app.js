316efb02c3307cff2eeda2729f085b76cfc713f1
KimHyeongSeop <gygy2006@naver.com>
2023-12-21 04:03:55 +0900

    google drive API file upload

--- app.js
+++ app.js
@@ -5,20 +5,38 @@ import readline from "readline";
 import multer from "multer";
 import mime from "mime";
 import dotenv from "dotenv";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 import { google } from "googleapis";
+import streamifier from "streamifier";
 
 dotenv.config();
-const storage = multer.memoryStorage({});
-
-const OAuth2 = google.auth.OAuth2;
-const upload = multer({ dest: "uploads/" });
-
 process.env.NODE_ENV =
   process.env.NODE_ENV && process.env.NODE_ENV.trim().toLowerCase() == "production" ? "production" : "development";
 
 export const app = express();
-
 export const __dirname = path.resolve();
+
+const allowlist = [
+  "https://storemap-389307.du.r.appspot.com",
+  "https://storemap.store",
+  "http://localhost:8080",
+  "https://localhost:8000",
+  "http://127.0.0.1:8080",
+  "https://127.0.0.1:8000",
+];
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+
+  callback(null, { origin: true, credentials: true }); // callback expects two parameters: error and options
+};
 
 const appInit = async () => {
   app.set("view engine", "ejs"); // 템플릿 엔진 설정
@@ -28,43 +46,162 @@ const appInit = async () => {
   app.use(express.urlencoded({ extended: true }));
   // 정적 파일 경로 설정 (선택사항)
   app.use(express.static(__dirname + "/public")); // apply css , js
+  app.use(cors(corsOptionsDelegate)); // cors setting
 };
 
 appInit();
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: __dirname + "/env/utilityapp-399403-24a680662fd9.json",
-  scopes: ["https://www.googleapis.com/auth/drive.file"],
-});
-
-const drive = google.drive({ version: "v3", auth });
-
 app.get("/", (req, res) => {
-  console.log(req);
   return res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/upload", upload.single("file"), async (req, res) => {
+const removeFileInServiceAuth = async (res) => {
   try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: req.file.originalname,
-        parents: [process.env.DRIVE_FORDER_ID],
-      },
-      media: {
-        mimeType: req.file.mimetype,
-        body: fs.createReadStream(req.file.path),
-      },
+    // 소유자의 이메일 주소
+    const serviceEmail = process.env.SERVICE_AUTH_EMAIL;
+
+    // 소유자의 파일 검색
+    const fileLisRes = await drive.files.list({
+      pageSize: 10,
+      fields: "nextPageToken, files(id, name)",
+    });
+    const files = fileLisRes.data.files;
+    if (files.length) {
+      console.log("Files:");
+      files.map(async (file) => {
+        console.log(`${file.name} (${file.id})`);
+        await drive.files.delete({
+          fileId: file.id,
+        });
+      });
+    } else {
+      console.log("No files found.");
+    }
+
+    return res.status(200);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send(err);
+  }
+};
+
+const storage = multer.memoryStorage({
+  filename: function (req, file, cb) {
+    // 파일 이름 인코딩
+    const id = uuidv4();
+    const ext = path.extname(file.originalname);
+    file.originalname = Buffer.from(file.originalname.replace(ext, ""), "latin1").toString("utf8") + "_" + id + ext;
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+const keys = JSON.parse(fs.readFileSync(__dirname + "/env/utilityapp-399403-feb51c49e333.json"));
+const auth = new google.auth.JWT(
+  keys.client_email,
+  null,
+  keys.private_key,
+  ["https://www.googleapis.com/auth/drive"],
+  process.env.OWNER_WORKSPACE_EMAIL // 도메인 전체 위임을 위한 이메일 주소
+);
+
+const drive = google.drive({ version: "v3", auth });
+
+const folderIdGetOrCraate = async (parentFolderId, folderName, folderResponse) => {
+  return new Promise(async (resolve, reject) => {
+    if (folderResponse.data.files.length > 0) {
+      // order_images 폴더안에 주문일 폴더가 이미 존재하는 경우
+      resolve(folderResponse.data.files[0].id);
+    } else {
+      try {
+        // 주문일 폴더가 존재하지 않는 경우, 주문날자로 새로운 폴더 생성
+        const folderMetadata = {
+          name: folderName,
+          mimeType: "application/vnd.google-apps.folder",
+          parents: [parentFolderId],
+        };
+
+        const folder = await drive.files.create({
+          resource: folderMetadata,
+          fields: "id",
+        });
+
+        const permissionsRes = await drive.permissions.create({
+          fileId: folder.data.id,
+          transferOwnership: true,
+          fields: "id",
+          resource: {
+            role: "owner",
+            type: "user",
+            emailAddress: ownerEmail,
+          },
+        });
+
+        console.log(permissionsRes);
+
+        resolve(folder.data.id);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    }
+  });
+};
+
+app.post("/upload", upload.array("files"), async (req, res) => {
+  try {
+    const { order_id } = req.query;
+    const files = req.files;
+
+    const firstFolderName = process.env.FIRST_PARENT_FOLDER_ID;
+    let secondFolderId, thirdFolderId; // secondFolderId = 주문일 thirdFolderId = 주문번호
+    const secondFolderName = order_id.split("-")[0]; // 주문일
+    const thirdFolderName = order_id; // 주문번호
+
+    // order_images 폴더 안에 주문일 폴더가 있는지 확인
+    const secondParentFolderSearchRes = await drive.files.list({
+      q: `${firstFolderName} in parents and mimeType='application/vnd.google-apps.folder' and name='${secondFolderName}'`,
+      spaces: "drive",
+      fields: "files(id, name)",
     });
 
-    res.send("File uploaded successfully.");
+    //
+    secondFolderId = await folderIdGetOrCraate(firstFolderName, secondFolderName, secondParentFolderSearchRes);
+
+    const thirdParentFolderSearchRes = await drive.files.list({
+      q: `'${secondFolderId}' in parents and mimeType='application/vnd.google-apps.folder' and name='${thirdFolderName}'`,
+      spaces: "drive",
+      fields: "files(id, name)",
+    });
+
+    thirdFolderId = await folderIdGetOrCraate(secondFolderId, thirdFolderName, thirdParentFolderSearchRes);
+
+    for (const file of files) {
+      const fileRes = await drive.files.create({
+        requestBody: {
+          name: file.originalname,
+          parents: [thirdFolderId],
+        },
+        media: {
+          mimeType: file.mimetype,
+          body: streamifier.createReadStream(file.buffer), // file.buffer를 스트림으로 변환합니다.
+        },
+        fields: "id",
+      });
+    }
+
+    console.log("file upload success");
+    return res.status(200).json({ ok: true });
   } catch (error) {
     console.log(error);
-    res.status(500).send("Failed to upload file.");
+
+    return res.status(500).json({ ok: false });
   }
 });
 
 app.post("/users/upload", upload.single("file"), async (req, res) => {
+  const OAuth2 = google.auth.OAuth2;
   // Load client secrets from a local file.
   fs.readFile(
     "./env/client_secret_209733163609-vg2b70am3talq49anjitjddujlssu4b5.apps.googleusercontent.com.json",
@@ -77,7 +214,7 @@ app.post("/users/upload", upload.single("file"), async (req, res) => {
 
   function authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.web;
-    console.log(redirect_uris);
+
     const oAuth2Client = new OAuth2(client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
@@ -140,5 +277,7 @@ app.post("/users/upload", upload.single("file"), async (req, res) => {
       console.error("The API returned an error: " + err);
       res.status(500).send("File upload failed.");
     }
+
+    fs.unlinkSync(req.file.path); // 업로드가 완료된 후 임시 파일 삭제
   }
 });
